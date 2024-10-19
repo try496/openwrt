d44ed51b764735b1d598677b12ff2442483a460b
try496 <pinghejk@gmail.com>
2024-10-19 11:52:00 +0800

    qualcommax: ipq60xx: add XiaoMi AX5-ZN-M2 support

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -52,3 +52,26 @@ define Device/yuncore_fap650
 endef
 TARGET_DEVICES += yuncore_fap650
 
+define Device/redmi_ax5
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Redmi
+	DEVICE_MODEL := AX5
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@cp03-c1
+	SOC := ipq6000
+endef
+TARGET_DEVICES += redmi_ax5
+
+define Device/zn_m2
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := ZN
+	DEVICE_MODEL := M2
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@cp03-c1
+	SOC := ipq6000
+endef
+TARGET_DEVICES += zn_m2
